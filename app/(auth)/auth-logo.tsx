0958534024c59a2511fc553847ed6df492cd2ace
BuildAIStartups <buildaistartups@gmail.com>
2025-07-26 22:58:44 +0200

    Update auth-logo.tsx

--- app/(auth)/auth-logo.tsx
+++ app/(auth)/auth-logo.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import Logo from '@/public/images/logo.svg'
+const logoSrc = '/images/logo.svg'
 
 export default function AuthLogo() {
   return (
