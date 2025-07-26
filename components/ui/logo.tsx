74f450ee1d4a602f1de356a3a1f2ff49d5cb8c7b
BuildAIStartups <buildaistartups@gmail.com>
2025-07-26 11:59:10 +0200

    Update logo.tsx

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -5,7 +5,7 @@ import LogoImg from '@/public/images/logo.svg'
 export default function Logo() {
   return (
     <Link className="inline-flex" href="/" aria-label="Cruip">
-      <Image className="max-w-none" src={LogoImg} width={38} height={38} priority alt="Stellar" />
+      <Image className="max-w-none" src={LogoImg} width={72} height={80} priority alt="Stellar" />
     </Link>
   )
 }
