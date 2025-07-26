d95bd6be0e5d0c421fba4cde63acf58b4aab5120
BuildAIStartups <buildaistartups@gmail.com>
2025-07-27 01:10:14 +0200

    Update logo.tsx

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Logo() {
   return (
     <Link className="inline-flex items-center" href="/" aria-label="BuildAI Startups">
-      <Image src="/images/logo.svg" width={48} height={48} alt="BuildAI Startups Logo" priority />
+      <Image src="/images/logo.svg" width={72} height={80} alt="BuildAI Startups Logo" priority />
       <span className="ml-2 font-bold text-xl text-white">BuildAI Startups</span>
     </Link>
   )
