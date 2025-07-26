bf927b6c718ccb1f362661be9fa4e1c90e3d5d0f
BuildAIStartups <buildaistartups@gmail.com>
2025-07-26 19:57:02 +0200

    Update logo.tsx

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -16,8 +16,8 @@ export default function Logo() {
       <Image
         className="max-w-none"
         src={logoSrc}
-        width={192}
-        height={200}
+        width={212}
+        height={220}
         priority
         alt="BuildAI Startups Logo"
       />
