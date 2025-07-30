04521d848f72b0d184b47a1080cfab527297cc46
BuildAIStartups <buildaistartups@gmail.com>
2025-07-30 20:27:54 +0200

    Update page.tsx

--- app/(default)/customers/page.tsx
+++ app/(default)/customers/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: 'Customers - Stellar',
+  title: 'Customers - BuildAIStartups.com',
   description: 'Page description',
 }
 
@@ -32,10 +32,10 @@ export default function Customers() {
 
             {/* Section header */}
             <div className="text-center pb-12 md:pb-20">
-              <div className="inline-flex font-medium bg-clip-text text-transparent bg-linear-to-r from-purple-500 to-purple-200 pb-3">Leaders love Stellar</div>
+              <div className="inline-flex font-medium bg-clip-text text-transparent bg-linear-to-r from-purple-500 to-purple-200 pb-3">Leaders love BuildAIStartups.com</div>
               <h1 className="h1 bg-clip-text text-transparent bg-linear-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">Trusted by leading companies</h1>
               <div className="max-w-3xl mx-auto">
-                <p className="text-lg text-slate-400">Stellar powers thousands of high-impact product teams. From next-gen startups who reach for the stars to established greats who change the world.</p>
+                <p className="text-lg text-slate-400">BuildAIStartups.com powers thousands of high-impact product teams. From next-gen startups who reach for the stars to established greats who change the world.</p>
               </div>
             </div>
 
