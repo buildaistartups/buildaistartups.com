98394071e8da99ecbd94f612c9444766ee97b6c8
BuildAIStartups <buildaistartups@gmail.com>
2025-07-30 21:41:03 +0200

    Update features-05.tsx

--- components/features-05.tsx
+++ components/features-05.tsx
@@ -16,7 +16,7 @@ export default function Features05() {
 
       {/* Particles animation */}
       <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10 w-64 h-64 -mt-24">
-        <Particles className="absolute inset-0 -z-10" quantity={6} staticity={30} />
+        <Particles className="absolute inset-0 -z-10" quantity={6} staticity={50} />
       </div>
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
