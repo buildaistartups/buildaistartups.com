e9666bf0dc5bc5365ccb78ff5de30cf482072712
BuildAIStartups <buildaistartups@gmail.com>
2025-07-31 23:25:19 +0200

    Update layout.tsx

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useEffect } from 'react'
-
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
@@ -21,18 +20,14 @@ export default function DefaultLayout({
       duration: 1000,
       easing: 'ease-out-cubic',
     })
-  })
+  }, [])
 
   return (
     <>
       <Header />
-      
       <main className="grow">
-
         {children}
-
       </main>
-
       <Footer />
     </>
   )
