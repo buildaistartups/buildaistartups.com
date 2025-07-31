adf47a3a042e5b7a56b64bbb729cbe2587b9a16d
BuildAIStartups <buildaistartups@gmail.com>
2025-07-31 23:04:43 +0200

    Update page.tsx

--- app/(default)/changelog/page.tsx
+++ app/(default)/changelog/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: 'Changelog - Stellar',
+  title: 'Changelog - BuildAIStartups.com',
   description: 'Page description',
 }
 
@@ -19,7 +19,6 @@ export default function Changelog() {
 
   return (
     <>
-
       {/* Content */}
       <section className="relative">
 
@@ -46,7 +45,9 @@ export default function Changelog() {
             <div className="text-center pb-12 md:pb-20">
               <h1 className="h1 bg-clip-text text-transparent bg-linear-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">What's New</h1>
               <div className="max-w-3xl mx-auto">
-                <p className="text-lg text-slate-400">New updates and improvements to Stellar.</p>
+                <p className="text-lg text-slate-400">
+                  New updates and improvements to <b>BuildAIStartups.com</b>.
+                </p>
               </div>
             </div>
 
