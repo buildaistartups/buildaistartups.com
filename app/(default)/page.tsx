d4f5cb8b79f29ba4a18fc8dee01913b791a7de10
BuildAIStartups <buildaistartups@gmail.com>
2025-08-01 22:24:55 +0200

    Update page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -27,9 +27,6 @@ export default function Home() {
       <Pricing />
       <Testimonials />
       <Cta />
-      <div className="bg-white text-black dark:bg-black dark:text-white p-4">
-  Test: This background should change with dark/light!
-</div>
     </>
   )
 }
