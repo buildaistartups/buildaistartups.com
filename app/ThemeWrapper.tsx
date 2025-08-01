142996d4c0f2d879240159fd5244fad69dc32493
BuildAIStartups <buildaistartups@gmail.com>
2025-08-01 10:18:23 +0200

    Update ThemeWrapper.tsx

--- app/ThemeWrapper.tsx
+++ app/ThemeWrapper.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
+import type { ReactNode } from 'react'
 
-export default function ThemeWrapper({ children }: { children: React.ReactNode }) {
+export default function ThemeWrapper({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       {children}
