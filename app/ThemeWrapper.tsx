00624798030cb0ded1c4b29d556de4a923e41e9d
BuildAIStartups <buildaistartups@gmail.com>
2025-08-11 22:49:53 +0200

    Update ThemeWrapper.tsx

--- app/ThemeWrapper.tsx
+++ app/ThemeWrapper.tsx
@@ -1,25 +1,11 @@
 'use client'
 
-import { ThemeProvider, useTheme } from 'next-themes'
-import dynamic from 'next/dynamic'
-
-// Match your on-disk filenames exactly (lowercase)
-const Features = dynamic(() => import('../components/features'), { ssr: false })
-const FeaturesLight = dynamic(() => import('../components/features-light'), { ssr: false })
-
-function FeaturesSwitcher() {
-  const { theme, resolvedTheme } = useTheme()
-  const active = theme ?? resolvedTheme
-  return active === 'light' ? <FeaturesLight /> : <Features />
-}
+import { ThemeProvider } from 'next-themes'
 
 export default function ThemeWrapper({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      <>
-        {children}
-        <FeaturesSwitcher />
-      </>
+      {children}
     </ThemeProvider>
   )
 }
