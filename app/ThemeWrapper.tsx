7e051985ed997fd8812bf4b602caedfecbdc1840
BuildAIStartups <buildaistartups@gmail.com>
2025-08-11 20:29:02 +0200

    Update ThemeWrapper.tsx

--- app/ThemeWrapper.tsx
+++ app/ThemeWrapper.tsx
@@ -1,11 +1,29 @@
 'use client'
 
-import { ThemeProvider } from 'next-themes'
+import { ThemeProvider, useTheme } from 'next-themes'
+import Features from './features'
+import FeaturesLight from './features-light'
+
+function FeaturesSwitcher() {
+  const { theme } = useTheme()
+
+  // Only swap component based on active theme
+  if (theme === 'light') {
+    return <FeaturesLight />
+  }
+  return <Features />
+}
 
 export default function ThemeWrapper({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      {children}
+      <>
+        {/* Render all other children */}
+        {children}
+
+        {/* Conditionally render the correct Features variant */}
+        <FeaturesSwitcher />
+      </>
     </ThemeProvider>
   )
 }
