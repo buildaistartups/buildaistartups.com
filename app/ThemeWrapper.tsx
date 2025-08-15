74de6b1cf512487a7fc436f02626679a9df0cc55
BuildAIStartups <buildaistartups@gmail.com>
2025-08-15 16:13:16 +0200

    Update ThemeWrapper.tsx

--- app/ThemeWrapper.tsx
+++ app/ThemeWrapper.tsx
@@ -1,11 +1,25 @@
 'use client'
 
-import { ThemeProvider } from 'next-themes'
+import { ThemeProvider, useTheme } from 'next-themes'
+import dynamic from 'next/dynamic'
+
+// Dynamic imports to match theme
+const TestimonialsCarousel = dynamic(() => import('../components/testimonials-carousel'), { ssr: false })
+const TestimonialsCarouselLight = dynamic(() => import('../components/testimonials-carousel-light'), { ssr: false })
+
+function TestimonialsSwitcher() {
+  const { theme, resolvedTheme } = useTheme()
+  const activeTheme = theme ?? resolvedTheme
+  return activeTheme === 'light' ? <TestimonialsCarouselLight /> : <TestimonialsCarousel />
+}
 
 export default function ThemeWrapper({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      {children}
+      <>
+        {children}
+        <TestimonialsSwitcher />
+      </>
     </ThemeProvider>
   )
 }
