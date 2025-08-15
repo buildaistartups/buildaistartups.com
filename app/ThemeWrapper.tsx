2a49de1b0238291edb8d8172e0922471b8b15ebe
BuildAIStartups <buildaistartups@gmail.com>
2025-08-15 16:37:28 +0200

    Update ThemeWrapper.tsx

--- app/ThemeWrapper.tsx
+++ app/ThemeWrapper.tsx
@@ -4,8 +4,6 @@ import { ThemeProvider, useTheme } from 'next-themes'
 import dynamic from 'next/dynamic'
 
 // Dynamic imports for theme-based components
-const Features = dynamic(() => import('../components/features'), { ssr: false })
-const FeaturesLight = dynamic(() => import('../components/features-light'), { ssr: false })
 
 const TestimonialsCarousel = dynamic(() => import('../components/testimonials-carousel'), { ssr: false })
 const TestimonialsCarouselLight = dynamic(() => import('../components/testimonials-carousel-light'), { ssr: false })
