022377246d55064f5aef0c9216aae45468c72fde
BuildAIStartups <buildaistartups@gmail.com>
2025-08-18 00:31:43 +0200

    Update ThemeAwarePricing.tsx

--- components/ThemeAwarePricing.tsx
+++ components/ThemeAwarePricing.tsx
@@ -1,1 +1,15 @@
+'use client'
 
+import { useTheme } from 'next-themes'
+import Pricing from '@/components/pricing'
+import PricingLight from '@/components/pricing-light'
+
+export default function ThemeAwarePricing() {
+  const { theme } = useTheme()
+
+  if (theme === 'light') {
+    return <PricingLight />
+  }
+
+  return <Pricing /> // default to dark
+}
