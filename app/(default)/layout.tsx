22e61ad89ce25fbd1d5268e3be5009e08e0c1e4c
BuildAIStartups <buildaistartups@gmail.com>
2025-09-21 04:31:07 +0200

    Update layout.tsx

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,3 +1,10 @@
+'use client'
+
+import { useEffect } from 'react'
+
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 import Header from '@/components/ui/header'
 import Footer from '@/components/ui/footer'
 
@@ -5,13 +12,27 @@ export default function DefaultLayout({
   children,
 }: {
   children: React.ReactNode
-}) {
+}) {  
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 1000,
+      easing: 'ease-out-cubic',
+    })
+  })
+
   return (
     <>
       <Header />
+      
       <main className="grow">
+
         {children}
+
       </main>
+
       <Footer />
     </>
   )
