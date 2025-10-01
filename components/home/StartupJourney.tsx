80d992a6c2bd5d92c846f51e6696e3f6da6e6499
BuildAIStartups <buildaistartups@gmail.com>
2025-10-02 00:38:13 +0200

    Update StartupJourney.tsx

--- components/home/StartupJourney.tsx
+++ components/home/StartupJourney.tsx
@@ -2,6 +2,7 @@
 'use client'
 
 import { useState } from 'react'
+import React from 'react'
 import Link from 'next/link'
 import { ArrowRight, Lightbulb, Code, Rocket, TrendingUp, DollarSign } from 'lucide-react'
 
