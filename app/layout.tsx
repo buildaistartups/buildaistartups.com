37034df3b2fdb3c474555ca77494be3eb7ffb063
BuildAIStartups <buildaistartups@gmail.com>
2025-12-30 15:42:18 +0100

    Import ThemeWrapper from components directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './css/style.css'
 
 import { Inter } from 'next/font/google'
-import ThemeWrapper from './ThemeWrapper'
+import ThemeWrapper from '@/components/ThemeWrapper'
 
 const inter = Inter({
   subsets: ['latin'],
