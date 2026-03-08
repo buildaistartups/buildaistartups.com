bf424dfa328cdfefe9b397de44b24a0f0fc80705
BuildAIStartups <buildaistartups@gmail.com>
2026-03-08 06:09:39 +0100

    Remove comments from next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@ const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-
-  // Turbopack top-level config (moved out of experimental.turbo)
-  turbopack: {},
 };
 
 module.exports = withMDX(nextConfig);
