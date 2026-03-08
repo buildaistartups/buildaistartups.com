0a9e108bdb1bb21ee4050a15458b108d1b50de08
BuildAIStartups <buildaistartups@gmail.com>
2026-03-08 13:16:17 +0100

    Integrate MDX support in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
+const withMDX = require("@next/mdx")();
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-}
- 
-module.exports = nextConfig
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+};
+
+module.exports = withMDX(nextConfig);
